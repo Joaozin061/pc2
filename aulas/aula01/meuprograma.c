e3eb90bc070e538aef79397af7a549ef0ac68a2e
Joaozin061 <142408566+Joaozin061@users.noreply.github.com>
2023-08-23 13:56:08 +0000

    envio para o github

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <math.h> 
+
+// funcao principal
+
+int main() {
+  printf("Ola pessoal!\n");
+  printf("raiz quadrada de 2 é igual a %f\n", sqrt(2));
+  return 0;
+} 
