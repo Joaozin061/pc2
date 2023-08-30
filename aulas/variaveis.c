e9206d00e7e9cecfcfb5bdd500fa68690ea83505
Joaozin061 <142408566+Joaozin061@users.noreply.github.com>
2023-08-30 13:43:15 +0000

    area

---
+++ aulas/variaveis.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+ 
+ char caracter = 0; // ou '\0' '\n', \'t' ; // 'a', 'b', 'c', -128 a 127 - 1 byte
+
+  caracter = 'a';
+  caracter = 'b';
+  
+
+ unsigned char caracter_sem_sinal; // 0 a 255
+
+ char string[10]; // "um texto"
+
+ char nome_da_variavel;
+
+ char i = 0;
+ char j = 0,
+ 
+ char string[10] = "um texto";
+
+ int inteiro; // -2.147483648 a 214748367 - 4 bytes
+  
+ unsigned int inteiro_sem_sinal; // 0 a 4294967295  
+ 
+ shor int inteiro_curto; // -32.768 a 32.767 - 2 bytes 
+
+ unsigned short int inteiro_curso_sem_sinal; // 0 a 65535
+
+ long int inteiro_longo; // 8 bytes -9223373036854775808 a 9223372036854775807
+
+ unsigned long int inteiro_longo_sem_sinal, // 0 a 
+  
+ 
+ float flutuante; // 0.123456f - 4 bytes
+
+  
+ double duplo; // 0.123456789012345 - 8 bytes
+
+  
+ void nada; // 1 byte
+
+ return 0;
+
+}
---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,27 @@
+Faça um programa em C que calcule a area de um triangulo. Considere a base e altura numeros inteiros maiores que 0;
+
+
+#include <studio.h>
+
+int main() }
+int base, altura, area;
+unsigned int (>0);
+
+printf("base do triangulo");
+ scanf("%f, %base") [6];
+  
+printf("altura do triangulo");
+ scanf("%f, &altura") [3];
+
+printf("a area do triangulo e \n, area")
+
+
+   
+
+
+
+return 0;
+
+} 
+
+
