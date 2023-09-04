5ba37db4be34b6ed61491b0a832b78d5b221b470
Joaozin061 <142408566+Joaozin061@users.noreply.github.com>
2023-09-04 13:54:22 +0000

    Códigos da aula 03

---
+++ aulas/aula02/constantes.c
@@ -0,0 +1,21 @@
+/* 
+Faça um programa em C que calcule o perímetro de uma piza (P = 2 X PI x r). Considere o raio um número inteiro e a constante PI igual a 3,1416.
+/*
+
+
+#include <stdio.h>
+
+#define PI 3.1416
+#define SEXO_MASCULINO 1
+
+int main() { 
+
+ float angulo = PI * 45; 
+ char sexo = SEXO_MASCULINO;
+ const char STATUS_ATIVO = 'a';
+ //  PI = 3.14; 
+// STATUS_ATIVO
+  
+ return 0;
+
+}
---
+++ aulas/aula02/operadores.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+  // + - x / => + - * /
+
+  int soma = 10 +  2;
+  int diferenca = 10 - 2; 
+  int multiplicacao = 10 * soma;
+  int divisao = (int) soma / 2.0f; // conversao explicita
+  int resto = soma % 2;
+
+  printf("soma = %i\n, soma");
+  printf("soma = %i\n, soma++"); // soma = soma + 1;
+  printf("soma = %i\n, soma")
+  printf("soma = %i\n, ++soma"); // soma = soma + 1;
+    
+  soma++; // soma = soma + 1;
+  ==soma;
+  soma--; // soma = soma - 1;
+  --soma;
+  
+
+  return 0; 
+    
+ }
