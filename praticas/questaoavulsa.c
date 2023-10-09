160cbdd95b7b2f63b47873b8418408359a83cafd
Joaozin061 <142408566+Joaozin061@users.noreply.github.com>
2023-10-09 19:05:41 +0000

    EXEMPLO AVULSO

---
+++ praticas/questaoavulsa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+  int main() {
+    printf("Múltiplos de 3 entre 1 e 100:\n");
+
+    
+
+   
+for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
+return 0; 
+}
